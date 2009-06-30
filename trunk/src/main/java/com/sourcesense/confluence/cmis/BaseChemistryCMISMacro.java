1f693bc830e8d4802ebaf3627791bbc16e32f6e2
ugo.cei <ugo.cei@83e5ee9e-5015-11de-ad91-37317df011ef>
2009-06-30 17:23:52 +0000

    Started refactoring to move away from Abdera and into Chemistry. Big WIP.

---
+++ trunk/src/main/java/com/sourcesense/confluence/cmis/BaseChemistryCMISMacro.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2009 Sourcesense <http://www.sourcesense.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sourcesense.confluence.cmis;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.apache.abdera.Abdera;
+import org.apache.chemistry.Repository;
+import org.apache.chemistry.atompub.client.ContentManager;
+import org.apache.chemistry.atompub.client.connector.APPContentManager;
+import org.apache.chemistry.xml.stax.StaxReader;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+
+import com.atlassian.bandana.BandanaManager;
+import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext;
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.macro.BaseMacro;
+import com.atlassian.renderer.v2.macro.MacroException;
+import com.sourcesense.confluence.cmis.configuration.ConfigureCMISPluginAction;
+
+public abstract class BaseChemistryCMISMacro extends BaseMacro {
+
+    private BandanaManager bandanaManager;
+    
+    public void setBandanaManager(BandanaManager bandanaManager) {
+        this.bandanaManager = bandanaManager;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public String execute(Map params, String body, RenderContext renderContext) throws MacroException {
+        
+        // Work around Abdera trying to be smart with class loading
+        ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            ClassLoader cl = this.getClass().getClassLoader();
+            Thread.currentThread().setContextClassLoader(cl);
+            
+            Abdera abdera = new Abdera();
+            
+            if (abdera.getParser() == null) {
+                return "NO PARSER!";
+            }
+        
+            try {
+                StaxReader reader = StaxReader.newReader(System.in);
+            } catch (XMLStreamException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+    
+            String serverUrl = (String) params.get("s");
+            String repositoryUsername = (String) params.get("u");
+            String repositoryPassword = (String) params.get("p");
+            UsernamePasswordCredentials credentials = getCredentials(serverUrl, repositoryUsername, repositoryPassword);
+            ContentManager cm = new APPContentManager(serverUrl);
+            cm.login(credentials.getUserName(), credentials.getPassword());
+            Repository repository = cm.getDefaultRepository();
+            return doExecute(params, body, renderContext, repository);
+        
+        } finally {
+            // Restore original classloader
+            Thread.currentThread().setContextClassLoader(originalClassLoader);
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    protected UsernamePasswordCredentials getCredentials(String url, String username, String password) {
+        if (username != null && password != null) {
+            return new UsernamePasswordCredentials(username , password);
+        }
+        Map<String, List<String>> credsMap = (Map<String, List<String>>) this.bandanaManager.getValue(new ConfluenceBandanaContext(), 
+                                        ConfigureCMISPluginAction.CREDENTIALS_KEY);
+        if (credsMap == null) {
+            return null;
+        }
+        for (String realm : credsMap.keySet()) {
+            if (url.startsWith(realm)) {
+                List<String> up = credsMap.get(realm);
+                return new UsernamePasswordCredentials(up.get(0), up.get(1));
+            }
+        }
+        return null;
+    }
+
+    protected abstract String doExecute(Map<String, String> params, String body, RenderContext renderContext, Repository repository) throws MacroException;
+}
--- trunk/src/main/java/com/sourcesense/confluence/cmis/SearchMacro.java
+++ trunk/src/main/java/com/sourcesense/confluence/cmis/SearchMacro.java
@@ -17,21 +17,18 @@ package com.sourcesense.confluence.cmis;
 
 
 
-import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.Map;
 
-import org.apache.abdera.i18n.iri.IRI;
-import org.apache.abdera.model.Document;
-import org.apache.abdera.model.Entry;
-import org.apache.abdera.model.Feed;
-import org.apache.abdera.protocol.client.AbderaClient;
-import org.apache.commons.httpclient.Credentials;
+import org.apache.chemistry.CMISObject;
+import org.apache.chemistry.Connection;
+import org.apache.chemistry.Repository;
 
 import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.macro.MacroException;
 
-public class SearchMacro extends BaseCMISMacro {
+public class SearchMacro extends BaseChemistryCMISMacro {
 
     public boolean isInline() {
         return false;
@@ -45,50 +42,36 @@ public class SearchMacro extends BaseCMISMacro {
         return null;
     }
 
-    protected String doExecute(Map<String, String> params, String body, RenderContext renderContext, AbderaClient client) throws MacroException {
-        String repositoryUsername = (String) params.get("u");
-        String repositoryPassword = (String) params.get("p");
-        String url = (String) params.get("s");
-        
-        Credentials credentials = getCredentials(url, repositoryUsername, repositoryPassword);
-        if (credentials != null) {
-            try {
-                client.addCredentials(null, null, null, credentials);
-            } catch (URISyntaxException e) {
-                throw new MacroException("Error during addCredentials for: " + repositoryUsername, e);
-            }
-        }
-
-        IRI queryCollectionURL = getQueryCollectionURL(url, client);
-        if (queryCollectionURL == null) {
-            throw new MacroException("No query collection found in Service Document");
-        }
-        Document<Feed> doc = doSearch(body, queryCollectionURL.toString(), client);
-        return renderFeed(doc.getRoot());
+    protected String doExecute(Map<String, String> params, String body, RenderContext renderContext, Repository repository) throws MacroException {
+        Connection conn = repository.getConnection(null);
+        Collection<CMISObject> res = conn.query(body, false);
+        return renderFeed(res);
     }
 
-    private String renderFeed(Feed feed) {
+    private String renderFeed(Collection<CMISObject> res) {
         StringBuilder out = new StringBuilder();
         out.append("||Title||Description||\n");
-        for (Entry entry : feed.getEntries()) {
+        for (CMISObject entry : res) {
             String url = null;
+            /*
             if (entry.getContentSrc() != null) {
                 url = entry.getContentSrc().toString();
             } else if(entry.getLink(CMISConstants.LINK_STREAM) != null) {
                 url = entry.getLink(CMISConstants.LINK_STREAM).getHref().toString();
             }
+            */
             out.append("|");
             if (url != null) {
                 out.append("[");
-                out.append(entry.getTitle());
+                out.append(entry.getName());
                 out.append("|");
                 out.append(url);
                 out.append("]");
             } else {
-                out.append(entry.getTitle());
+                out.append(entry.getName());
             }
             out.append("|");
-            out.append(entry.getSummary() != null ? entry.getSummary() : " ");
+            // out.append(entry.getSummary() != null ? entry.getSummary() : " ");
             out.append("|\n");
         }
         return out.toString();
