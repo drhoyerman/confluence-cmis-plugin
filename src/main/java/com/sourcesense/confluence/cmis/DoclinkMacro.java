e057d3e3fb923c948787d717c1ec88ab9ad36ff0
ugo.cei <ugo.cei@83e5ee9e-5015-11de-ad91-37317df011ef>
2009-07-09 15:21:07 +0000

    Refactored Doclink macro to use Chemistry APIs

--- src/main/java/com/sourcesense/confluence/cmis/DoclinkMacro.java
+++ src/main/java/com/sourcesense/confluence/cmis/DoclinkMacro.java
@@ -15,22 +15,17 @@
  */
 package com.sourcesense.confluence.cmis;
 
-import java.net.URISyntaxException;
+import java.net.URI;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.abdera.model.Document;
-import org.apache.abdera.model.Entry;
-import org.apache.abdera.protocol.client.AbderaClient;
-import org.apache.abdera.protocol.client.ClientResponse;
-import org.apache.commons.httpclient.Credentials;
+import org.apache.chemistry.CMISObject;
+import org.apache.chemistry.Repository;
 
 import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.macro.MacroException;
 
-public class DoclinkMacro extends BaseCMISMacro {
+public class DoclinkMacro extends BaseChemistryCMISMacro {
 
     public boolean isInline() {
         return true;
@@ -44,50 +39,20 @@ public class DoclinkMacro extends BaseCMISMacro {
         return null;
     }
 
-    @SuppressWarnings("unchecked")
-    public String doExecute(Map params, String body, RenderContext renderContext, AbderaClient client) throws MacroException {
-        String repositoryUsername = (String) params.get("u");
-        String repositoryPassword = (String) params.get("p");
-        String url = (String) params.get("s");
+    protected String doExecute(Map<String, String> params, String body, RenderContext renderContext, Repository repository) throws MacroException {
         String id = (String) params.get("id");
-        
-        Credentials credentials = getCredentials(url, repositoryUsername, repositoryPassword);
-        if (credentials != null) {
-            try {
-                client.addCredentials(null, null, null, credentials);
-            } catch (URISyntaxException e) {
-                throw new MacroException("Error during addCredentials for: " + repositoryUsername, e);
-            }
+        CMISObject obj = getEntryViaID(repository, id);
+        if (obj == null) {
+            throw new MacroException("No such object: " + id);
         }
-        
-        if (id != null) { // Search by ObjectId
-            Entry entry = getEntryViaID(client, url, id);
-            if (entry != null) {
-                return renderEntry(entry);
-            } else {
-                throw new MacroException("No such document");
-            }
-        } else { // Fetch entry via its URI
-            ClientResponse clientResponse = client.get(url);
-            if (clientResponse.getStatus() == HttpServletResponse.SC_OK) {
-                Document<Entry> doc = clientResponse.getDocument();
-                return renderEntry(doc.getRoot());
-            } else {
-                throw new MacroException(clientResponse.getStatus() + " " + clientResponse.getStatusText());
-            }
-        }
+        return renderEntry(obj);
     }
 
-    private String renderEntry(Entry entry) {
+    private String renderEntry(CMISObject entry) {
         StringBuilder out = new StringBuilder();
-        String url = null;
-        if (entry.getContentSrc() != null) {
-            url = entry.getContentSrc().toString();
-        } else if(entry.getLink(CMISConstants.LINK_STREAM) != null) {
-            url = entry.getLink(CMISConstants.LINK_STREAM).getHref().toString();
-        }
+        URI url = entry.getURI("ContentStreamUri"); // XXX Should there be a constant definition in CMIS class for this?
         out.append("[");
-        out.append(entry.getTitle());
+        out.append(entry.getName());
         out.append("|");
         out.append(url);
         out.append("]");
