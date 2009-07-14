dfc53ad231bfb50c288b6a7c62d00461d49dd563
ugo.cei <ugo.cei@83e5ee9e-5015-11de-ad91-37317df011ef>
2009-07-14 14:13:33 +0000

    Refactored DocInfo macro to use Chemistry APIs

--- src/main/java/com/sourcesense/confluence/cmis/DocinfoMacro.java
+++ src/main/java/com/sourcesense/confluence/cmis/DocinfoMacro.java
@@ -15,25 +15,18 @@
  */
 package com.sourcesense.confluence.cmis;
 
-import java.net.URISyntaxException;
-import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.abdera.model.Document;
-import org.apache.abdera.model.Element;
-import org.apache.abdera.model.Entry;
-import org.apache.abdera.model.ExtensibleElement;
-import org.apache.abdera.protocol.client.AbderaClient;
-import org.apache.abdera.protocol.client.ClientResponse;
-import org.apache.commons.httpclient.Credentials;
+import org.apache.chemistry.CMISObject;
+import org.apache.chemistry.Property;
+import org.apache.chemistry.PropertyType;
+import org.apache.chemistry.Repository;
 
 import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.macro.MacroException;
 
-public class DocinfoMacro extends BaseCMISMacro {
+public class DocinfoMacro extends BaseChemistryCMISMacro {
 
     public boolean isInline() {
         return false;
@@ -47,75 +40,33 @@ public class DocinfoMacro extends BaseCMISMacro {
         return null;
     }
 
-    @SuppressWarnings("unchecked")
-    public String doExecute(Map params, String body, RenderContext renderContext, AbderaClient client) throws MacroException {
-        String repositoryUsername = (String) params.get("u");
-        String repositoryPassword = (String) params.get("p");
-        String url = (String) params.get("s");
+    protected String doExecute(Map<String, String> params, String body, RenderContext renderContext, Repository repository) throws MacroException {
         String id = (String) params.get("id");
-        
-        Credentials credentials = getCredentials(url, repositoryUsername, repositoryPassword);
-        if (credentials != null) {
-            try {
-                client.addCredentials(null, null, null, credentials);
-            } catch (URISyntaxException e) {
-                throw new MacroException("Error during addCredentials for: " + repositoryUsername, e);
-            }
+        CMISObject obj = getEntryViaID(repository, id);
+        if (obj == null) {
+            throw new MacroException("No such object: " + id);
         }
-        
-        if (id != null) { // Search by ObjectId
-            Entry entry = getEntryViaID(client, url, id);
-            if (entry != null) {
-                return renderEntry(entry);
-            } else {
-                throw new MacroException("No such document");
-            }
-        } else { // Fetch entry via its URI
-            ClientResponse clientResponse = client.get(url);
-            if (clientResponse.getStatus() == HttpServletResponse.SC_OK) {
-                Document<Entry> doc = clientResponse.getDocument();
-                return renderEntry(doc.getRoot());
-            } else {
-                throw new MacroException(clientResponse.getStatus() + " " + clientResponse.getStatusText());
-            }
-        }
+        return renderInfo(obj, repository);
     }
 
-    private String renderEntry(Entry entry) {
+    private String renderInfo(CMISObject cmisObject, Repository repository) {
         StringBuilder out = new StringBuilder();
         out.append("||Property||Value||\n");
-        ExtensibleElement cmisObject = entry.getExtension(CMISConstants.OBJECT);
-        if (cmisObject != null) {
-            ExtensibleElement cmisProperties = cmisObject.getExtension(CMISConstants.PROPERTIES);
-            if (cmisProperties != null) {
-                List<Element> cmisProps = cmisProperties.getElements();
-                for (Element prop : cmisProps) {
-                    
-                    System.err.println(prop.getQName());
-                    
-                    if (!CMISConstants.CMIS_NS_URI.equals(prop.getQName().getNamespaceURI())) {
-                        continue;
-                    }
-                    String name = prop.getAttributeValue(CMISConstants.NAME);
-                    if (name == null) {
-                        continue;
-                    }
-                    Element cmisValue = ((ExtensibleElement) prop).getExtension(CMISConstants.VALUE);
-                    if (cmisValue != null) {
-                        String value = cmisValue.getText();
-                        if (CMISConstants.PROPERTY_BOOLEAN.equals(prop.getQName())) {
-                            value = "true".equalsIgnoreCase(value) ? "(/)" : "(x)";
-                        } else if (CMISConstants.PROPERTY_URI.equals(prop.getQName())) {
-                            value = "[LINK|" + value + "]";
-                        }
-                        out.append("|");
-                        out.append(name);
-                        out.append("|");
-                        out.append(value);
-                        out.append("|\n");
-                    }
-                }
+        for (String name : cmisObject.getProperties().keySet()) {
+            Property property = cmisObject.getProperties().get(name);
+            String value = " ";
+            if (PropertyType.BOOLEAN.equals(property.getDefinition().getType())) {
+                value = Boolean.TRUE.equals(property.getValue()) ? "(/)" : "(x)";
+            } else if (PropertyType.URI.equals(property.getDefinition().getType())) {
+                value = "[LINK|" + property.getValue() + "]";
+            } else if (property.getValue() != null) {
+                value = property.getValue().toString();
             }
+            out.append("|");
+            out.append(name);
+            out.append("|");
+            out.append(value);
+            out.append("|\n");
         }
         return out.toString();
     }
