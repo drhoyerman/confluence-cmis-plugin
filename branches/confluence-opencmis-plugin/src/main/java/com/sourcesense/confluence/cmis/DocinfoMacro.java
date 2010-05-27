161eb387fe67f8c2cfdc4d29ac5b0667efe65c7b
c.sciolla@sourcesense.com <c.sciolla@sourcesense.com@83e5ee9e-5015-11de-ad91-37317df011ef>
2010-05-27 17:51:28 +0000

    First working OpenCMIS implementation

--- branches/confluence-opencmis-plugin/src/main/java/com/sourcesense/confluence/cmis/DocinfoMacro.java
+++ branches/confluence-opencmis-plugin/src/main/java/com/sourcesense/confluence/cmis/DocinfoMacro.java
@@ -15,9 +15,21 @@
  */
 package com.sourcesense.confluence.cmis;
 
+import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
+import org.apache.chemistry.opencmis.client.api.Document;
+import org.apache.chemistry.opencmis.client.api.ObjectId;
+import org.apache.chemistry.opencmis.client.api.Property;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
+import org.apache.chemistry.opencmis.commons.data.PropertyId;
+
+import java.util.List;
+import java.util.Map;
 
 public class DocinfoMacro extends BaseCMISMacro {
+
+    public static final String PARAM_DOCUMENT_ID = "id";
 
     public boolean hasBody() {
         return false;
@@ -63,4 +75,31 @@ public class DocinfoMacro extends BaseCMISMacro {
     }
 */
 
+    @Override
+    protected String executeImpl(Map params, String body, RenderContext renderContext, Session session)
+    {
+        String documentId = (String)params.get(PARAM_DOCUMENT_ID);
+        ObjectId objectId = session.createObjectId(documentId);
+
+        Document document = (Document)session.getObject(objectId);
+
+        return renderDocument (document);  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    protected String renderDocument(Document document)
+    {
+        List<Property<?>> properties = document.getProperties();
+        Property<String> title = document.getProperty(PropertyIds.NAME);
+
+        StringBuffer sb = new StringBuffer (String.format ("*Details of %s*\n", title.getValueAsString()));
+
+        sb.append ("||Property||Value||\n");
+        for (Property<?> prop : properties)
+        {
+            // TODO: handle different propertyTypes
+            sb.append (String.format ("|%s|%s|\n", prop.getDisplayName(), prop.getValueAsString()));
+        }
+
+        return sb.toString ();
+    }
 }
