b3350562ecee944999a0f71e1789dfd12397d3ae
lelelombardi@libero.it <lelelombardi@libero.it@83e5ee9e-5015-11de-ad91-37317df011ef>
2010-05-28 13:50:42 +0000

    utils 2 opencmis

--- branches/confluence-opencmis-plugin/src/main/java/com/sourcesense/confluence/cmis/utils/Utils.java
+++ branches/confluence-opencmis-plugin/src/main/java/com/sourcesense/confluence/cmis/utils/Utils.java
@@ -1,26 +1,21 @@
 package com.sourcesense.confluence.cmis.utils;
 
+import java.net.URI;
+
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.enums.BaseTypeId;
+
 import com.atlassian.confluence.setup.settings.SettingsManager;
 import com.atlassian.spring.container.ContainerManager;
 import com.sourcesense.confluence.servlets.CMISProxyServlet;
 
-import java.net.URI;
-
 public class Utils {
 
-/*
-    public static ObjectEntry getEntryViaID(Repository repository, String id, BaseType type) {
-        Type t = repository.getType(type.toString());
-        String cmisQuery = "SELECT * FROM " + t.getBaseTypeQueryName() + " WHERE ObjectId = '" + id + "'"; //cmis:document is the actual (0.62Spec) common query name
-        Connection conn = repository.getConnection(null);
-        SPI spi = conn.getSPI();
-        Collection<ObjectEntry> res = spi.query(cmisQuery, false, false, false, 1, 0, new boolean[1]);
-        for (ObjectEntry entry : res) {
-            return entry;
-        }
-        return null;
+
+    public static CmisObject getEntryViaID(Session session, String id, BaseTypeId baseType) {
+        return  session.getObject(session.createObjectId(id));
     }
-*/
 
     public static String getBaseUrl() {
         SettingsManager settingsManager = (SettingsManager) ContainerManager.getComponent("settingsManager");
