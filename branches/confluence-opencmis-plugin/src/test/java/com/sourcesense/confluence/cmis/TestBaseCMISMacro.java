d17a6809e6646de8a25be457f9b37288c3ed1e74
pillitu.maurizio <pillitu.maurizio@83e5ee9e-5015-11de-ad91-37317df011ef>
2010-06-04 10:16:47 +0000

    Fixed test; default config go on cmis.alfresco.com; using system prop, you can override the defaults

--- branches/confluence-opencmis-plugin/src/test/java/com/sourcesense/confluence/cmis/TestBaseCMISMacro.java
+++ branches/confluence-opencmis-plugin/src/test/java/com/sourcesense/confluence/cmis/TestBaseCMISMacro.java
@@ -1,21 +1,19 @@
+/*
+ * Copyright 2009 Sourcesense <http://www.sourcesense.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sourcesense.confluence.cmis;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.WeakHashMap;
-
-import junit.framework.TestCase;
-
-import org.apache.chemistry.opencmis.client.api.CmisObject;
-import org.apache.chemistry.opencmis.client.api.Document;
-import org.apache.chemistry.opencmis.client.api.ItemIterable;
-import org.apache.chemistry.opencmis.client.api.Repository;
-import org.apache.chemistry.opencmis.client.api.Session;
-import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
-import org.apache.log4j.Logger;
 
 import com.atlassian.bandana.BandanaContext;
 import com.atlassian.bandana.BandanaManager;
@@ -23,159 +21,150 @@ import com.atlassian.renderer.RenderContext;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.macro.MacroException;
 import com.sourcesense.confluence.cmis.utils.RepositoryStorage;
+import junit.framework.TestCase;
+import org.apache.chemistry.opencmis.client.api.*;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
+import org.apache.log4j.Logger;
 
-/**
- * @author Carlo Sciolla &lt;c.sciolla@sourcesense.com&gt;
- */
-public class TestBaseCMISMacro extends TestCase
-{
-    Logger logger = Logger.getLogger(TestBaseCMISMacro.class);
+import java.util.*;
 
-    public void testRepositoryConnection() throws Exception
-    {
-        Map<String, String> parameters = new HashMap<String, String>();
-//        parameters.put("id", "alfresco");
-        parameters.put("n", "alfresco");
+public class TestBaseCMISMacro extends TestCase {
+  Logger logger = Logger.getLogger(TestBaseCMISMacro.class);
 
-        BaseCMISMacro baseMacro = new BaseCMISMacro()
-        {
+  String cmisRealm = "http://cmis.alfresco.com:80/service/cmis";
+  String cmisUser = "admin";
+  String cmisPwd = "admin";
 
-            public boolean hasBody()
-            {
-                return false;  //To change body of implemented methods use File | Settings | File Templates.
-            }
+  public void testRepositoryConnection() throws Exception {
+    Map<String, String> parameters = new HashMap<String, String>();
+    parameters.put("n", "test");
 
-            public RenderMode getBodyRenderMode()
-            {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
-            }
+    /**
+         * You can use -Drealm -Duser and -Dpwd in order to override the default values
+       **/
+    String cmisRealmProp = System.getProperty("realm");
+    if (cmisRealmProp != null) cmisRealm = cmisRealmProp;
+    String cmisUserProp = System.getProperty("user");
+    if (cmisUserProp != null) cmisUser = cmisUserProp;
+    String cmisPwdProp = System.getProperty("pwd");
+    if (cmisPwdProp != null) cmisPwd = cmisPwdProp;
 
-            @Override
-            protected String executeImpl(Map params, String body, RenderContext renderContext, Repository repo)
-            {
-                return "OK";
-            }
-        };
+    BaseCMISMacro baseMacro = new BaseCMISMacro() {
 
-        baseMacro.setBandanaManager(new MockBandanaManager());
+      public boolean hasBody() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+      }
 
-        String result = null;
+      public RenderMode getBodyRenderMode() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+      }
 
-        try
-        {
-            result = baseMacro.execute(parameters, null, null);
-        }
-        catch (MacroException me)
-        {
-            logger.error(me);
-            fail(me.getMessage());
-        }
+      @Override
+      protected String executeImpl(Map params, String body, RenderContext renderContext, Repository repo) {
+        return "OK";
+      }
+    };
 
-        assertNotNull(result);
-        assertFalse("".equals(result));
+    baseMacro.setBandanaManager(new MockBandanaManager());
 
-        logger.debug(result);
+    String result = null;
+
+    try {
+      result = baseMacro.execute(parameters, null, null);
+    }
+    catch (MacroException me) {
+      logger.error(me);
+      fail(me.getMessage());
     }
 
-    public void testRepositoryEnumeration()
-    {
+    assertNotNull(result);
+    assertFalse("".equals(result));
 
-        RepositoryStorage repoStorage = RepositoryStorage.getInstance(new MockBandanaManager());
+    logger.debug(result);
+  }
 
-        Set<String> repos = repoStorage.getRepositoryNames();
+  public void testRepositoryEnumeration() {
 
-        assertNotNull(repos);
-        assertTrue(!repos.isEmpty());
+    RepositoryStorage repoStorage = RepositoryStorage.getInstance(new MockBandanaManager());
 
-        for (String repo : repos)
-        {
-            try
-            {
-                Repository repoDesc = repoStorage.getRepository(repo);
+    Set<String> repos = repoStorage.getRepositoryNames();
 
-                logger.debug("name: " + repoDesc.getName());
-                logger.debug("id: " + repoDesc.getId());
-                logger.debug("productName : " + repoDesc.getProductName());
-                logger.debug("cmisVersionSupported: " + repoDesc.getCmisVersionSupported());
-                logger.debug("description: " + repoDesc.getDescription());
-            }
-            catch (CmisRuntimeException e)
-            {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                fail(e.getMessage());
-            }
-        }
+    assertNotNull(repos);
+    assertTrue(!repos.isEmpty());
 
+    for (String repo : repos) {
+      try {
+        Repository repoDesc = repoStorage.getRepository(repo);
+
+        logger.debug("name: " + repoDesc.getName());
+        logger.debug("id: " + repoDesc.getId());
+        logger.debug("productName : " + repoDesc.getProductName());
+        logger.debug("cmisVersionSupported: " + repoDesc.getCmisVersionSupported());
+        logger.debug("description: " + repoDesc.getDescription());
+      }
+      catch (CmisRuntimeException e) {
+        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        fail(e.getMessage());
+      }
     }
 
-    public void testCMISLinkGeneration()
-    {
-        RepositoryStorage repoStorage = RepositoryStorage.getInstance(new MockBandanaManager());
+  }
 
-        try
-        {
-            Repository repo = repoStorage.getRepository("alfresco");
-            Session session = repo.createSession();
-            ItemIterable<CmisObject> children = session.getRootFolder().getChildren();
-            for (CmisObject obj : children)
-            {
-                if ("TODO.txt".equals (obj.getName()))
-                {
-                    Document doc = (Document)obj;
-                    System.out.println (doc);
-                }
-            }
+  public void testCMISLinkGeneration() {
+    RepositoryStorage repoStorage = RepositoryStorage.getInstance(new MockBandanaManager());
+
+    try {
+      Repository repo = repoStorage.getRepository("test");
+      Session session = repo.createSession();
+      ItemIterable<CmisObject> children = session.getRootFolder().getChildren();
+      for (CmisObject obj : children) {
+        if ("TODO.txt".equals(obj.getName())) {
+          Document doc = (Document) obj;
+          System.out.println(doc);
         }
-        catch (CmisRuntimeException e)
-        {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            fail (e.getMessage());
-        }
+      }
+    }
+    catch (CmisRuntimeException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+      fail(e.getMessage());
+    }
+  }
+
+  class MockBandanaManager implements BandanaManager {
+
+    public void init() {
+      //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    class MockBandanaManager implements BandanaManager
-    {
-
-        public void init()
-        {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public void setValue(BandanaContext bandanaContext, String s, Object o)
-        {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        // used in RepositoryStorage
-
-        public Object getValue(BandanaContext bandanaContext, String s)
-        {
-            Map<String, List<String>> repoConfigs = new WeakHashMap<String, List<String>>();
-            List<String> repoConfig = new ArrayList<String>();
-
-            //repoConfig.add("http://cmis.alfresco.com:80/service/cmis");
-            repoConfig.add("http://localhost:8081/alfresco/service/cmis");
-            repoConfig.add("admin");
-            repoConfig.add("admin");
-
-            repoConfigs.put("alfresco", repoConfig);
-
-            return repoConfigs;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public Object getValue(BandanaContext bandanaContext, String s, boolean b)
-        {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public String exportValues(BandanaContext bandanaContext)
-        {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        public void importValues(BandanaContext bandanaContext, String s)
-        {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
+    public void setValue(BandanaContext bandanaContext, String s, Object o) {
+      //To change body of implemented methods use File | Settings | File Templates.
     }
+
+    // used in RepositoryStorage
+
+    public Object getValue(BandanaContext bandanaContext, String s) {
+      Map<String, List<String>> repoConfigs = new WeakHashMap<String, List<String>>();
+      List<String> repoConfig = new ArrayList<String>();
+
+      repoConfig.add(cmisRealm);
+      repoConfig.add(cmisUser);
+      repoConfig.add(cmisPwd);
+
+      repoConfigs.put("test", repoConfig);
+
+      return repoConfigs;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Object getValue(BandanaContext bandanaContext, String s, boolean b) {
+      return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public String exportValues(BandanaContext bandanaContext) {
+      return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void importValues(BandanaContext bandanaContext, String s) {
+      //To change body of implemented methods use File | Settings | File Templates.
+    }
+  }
 }
