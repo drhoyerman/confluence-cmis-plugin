c8d577c6ae2fa288fefe3372dd961236e9bae259
lelelombardi@libero.it <lelelombardi@libero.it@83e5ee9e-5015-11de-ad91-37317df011ef>
2010-06-04 11:08:28 +0000

    initial commit of folderExplorerMacro

---
+++ branches/confluence-opencmis-plugin/src/main/java/com/sourcesense/confluence/cmis/FolderExplorerMacro.java
@@ -0,0 +1,43 @@
+package com.sourcesense.confluence.cmis;
+
+import java.math.BigInteger;
+import java.util.Map;
+
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Repository;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.data.ObjectInFolderList;
+
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.RenderMode;
+import com.atlassian.renderer.v2.macro.MacroException;
+
+public class FolderExplorerMacro extends BaseCMISMacro {
+  
+  public static final String PARAM_FOLDER_ID = "id";
+  public static final String PARAM_RESULTS_NUMBER = "rn";
+
+  @Override
+  protected String executeImpl(Map params, String body, RenderContext renderContext,
+          Repository repository) throws MacroException {
+    Session session = repository.createSession();
+    String folderId = (String) params.get(PARAM_FOLDER_ID);
+    int number = Integer.parseInt((String) params.get(PARAM_RESULTS_NUMBER));
+    
+    ObjectInFolderList list =session.getBinding().getNavigationService().getChildren(repository.getId(), folderId, null, null, false, null, null, null, BigInteger.valueOf(number), BigInteger.ZERO, null);
+    CmisObject o =session.getObject(session.createObjectId(list.getObjects().get(0).getObject().getId()));
+    
+
+    
+    return null;
+  }
+
+  public RenderMode getBodyRenderMode() {
+    return null;
+  }
+
+  public boolean hasBody() {
+    return false;
+  }
+
+}
