286daa9acac2e9e508b216f9de6c42917bfe49f8
pillitu.maurizio <pillitu.maurizio@83e5ee9e-5015-11de-ad91-37317df011ef>
2010-06-05 11:37:40 +0000

    switching trunk to opencmis implementation

---
+++ src/main/java/com/sourcesense/confluence/cmis/BaseCMISMacro.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2010 Sourcesense <http://www.sourcesense.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sourcesense.confluence.cmis;
+
+import com.atlassian.bandana.BandanaManager;
+import com.atlassian.confluence.setup.settings.SettingsManager;
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.RenderMode;
+import com.atlassian.renderer.v2.macro.BaseMacro;
+import com.atlassian.renderer.v2.macro.MacroException;
+import com.sourcesense.confluence.cmis.utils.ConfluenceCMISRepository;
+import com.sourcesense.confluence.cmis.utils.RepositoryStorage;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
+import org.apache.log4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+// TODO: put back in place the subclass delegation of behavior
+
+public abstract class BaseCMISMacro extends BaseMacro {
+  protected static final Logger logger = Logger.getLogger(BaseCMISMacro.class);
+
+  protected static final String PARAM_REPOSITORY_ID = "servername";
+  protected static final String PARAM_ID = "id";
+  protected static final String PARAM_RESULTS_NUMBER = "maxResults";
+  protected static final String PARAM_NOFORMAT = "nf";
+  protected static final String PARAM_USEPROXY = "useproxy";
+  protected static final String PARAM_PROPERTIES = "properties";
+
+  public static final java.lang.String REPOSITORY_NAME = "com.sourcesense.confluence.cmis.repository.name";
+
+  protected static final int DEFAULT_RESULTS_NUMBER = 20;
+  protected static final String DEFAULT_USEPROXY = "yes";
+
+  //TODO : make this regexp configurable, along with the locale
+  protected static SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d, ''yy");
+
+  protected BandanaManager bandanaManager;
+  protected SettingsManager settingsManager;
+
+  public void setBandanaManager(BandanaManager bandanaManager) {
+    this.bandanaManager = bandanaManager;
+  }
+
+  public void setSettingsManager(SettingsManager settingsManager) {
+    this.settingsManager = settingsManager;
+  }
+
+  @SuppressWarnings("unchecked")
+  public String execute(Map params, String body, RenderContext renderContext) throws MacroException {
+    // Work around to let OpenCMIS using the bundled version of Jaxb
+    ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
+
+    try {
+      ClassLoader cl = this.getClass().getClassLoader();
+      Thread.currentThread().setContextClassLoader(cl);
+
+      RepositoryStorage repositoryStorage = RepositoryStorage.getInstance(bandanaManager);
+      ConfluenceCMISRepository repositoryConfluence = getRepositoryFromParams(params, repositoryStorage);
+
+      populateParams(params, body, renderContext, repositoryConfluence);
+
+      return executeImpl(params, body, renderContext, repositoryConfluence);
+    }
+    catch (Exception e) {
+      logger.error("Cannot open a session with the CMIS repository", e);
+      e.printStackTrace();
+      throw new MacroException(e);
+    }
+    finally {
+      Thread.currentThread().setContextClassLoader(originalClassLoader);
+    }
+  }
+
+  /**
+   * Parses and provides common casting for mosly used and wide-spread macro parameters
+   * @param params
+   * @param body
+   * @param renderContext
+   * @param repositoryConfluence
+   */
+  private void populateParams(Map params, String body, RenderContext renderContext, ConfluenceCMISRepository repositoryConfluence) {
+    String useProxy = (String) params.get(BaseCMISMacro.PARAM_USEPROXY);
+    if (useProxy == null || useProxy.isEmpty()) {
+      useProxy = BaseCMISMacro.DEFAULT_USEPROXY;
+    }
+    if (useProxy.equals("y") || useProxy.equals("yes")) {
+      params.put(BaseCMISMacro.PARAM_USEPROXY, Boolean.TRUE);
+    } else {
+      params.put(BaseCMISMacro.PARAM_USEPROXY, Boolean.FALSE);
+    }
+  }
+
+  protected abstract String executeImpl(Map params, String body, RenderContext renderContext, ConfluenceCMISRepository repositoryConfluence) throws MacroException;
+  
+  /**
+   * Retrieves a Repository descriptor depending by the macro parameters:
+   * - The user must provide the repository id ("servername") than the repository details are taken from the plugin configuration
+   *
+   * @param params            Parameter map holding the macro parameters
+   * @param repositoryStorage Cached repository retriever
+   * @return
+   * @throws CmisRuntimeException
+   */
+  protected ConfluenceCMISRepository getRepositoryFromParams(Map params, RepositoryStorage repositoryStorage) throws CmisRuntimeException {
+    String repoId = (String) params.get(PARAM_REPOSITORY_ID);
+    if (repoId != null && !repoId.isEmpty()) {
+      return repositoryStorage.getRepository(repoId);
+    } else return repositoryStorage.getRepository();
+  }
+
+  public RenderMode getBodyRenderMode() {
+    return null;
+  }
+
+  public boolean hasBody() {
+    return false;
+  }
+  
+}
---
+++ src/test/java/com/sourcesense/confluence/cmis/TestBaseCMISMacro.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2010 Sourcesense <http://www.sourcesense.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sourcesense.confluence.cmis;
+
+import com.atlassian.bandana.BandanaContext;
+import com.atlassian.bandana.BandanaManager;
+import com.atlassian.renderer.RenderContext;
+import com.atlassian.renderer.v2.RenderMode;
+import com.atlassian.renderer.v2.macro.MacroException;
+import com.sourcesense.confluence.cmis.utils.ConfluenceCMISRepository;
+import com.sourcesense.confluence.cmis.utils.RepositoryStorage;
+import junit.framework.TestCase;
+import org.apache.chemistry.opencmis.client.api.*;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
+import org.apache.log4j.Logger;
+
+import java.util.*;
+
+public class TestBaseCMISMacro extends TestCase {
+  Logger logger = Logger.getLogger(TestBaseCMISMacro.class);
+
+  String cmisRealm = "http://cmis.alfresco.com:80/service/cmis";
+  String cmisUser = "admin";
+  String cmisPwd = "admin";
+
+  public void testRepositoryConnection() throws Exception {
+    Map<String, String> parameters = new HashMap<String, String>();
+    parameters.put("servername", "test");
+
+    /**
+     * You can use -Drealm -Duser and -Dpwd in order to override the default values
+     **/
+    String cmisRealmProp = System.getProperty("realm");
+    if (cmisRealmProp != null) cmisRealm = cmisRealmProp;
+    String cmisUserProp = System.getProperty("user");
+    if (cmisUserProp != null) cmisUser = cmisUserProp;
+    String cmisPwdProp = System.getProperty("pwd");
+    if (cmisPwdProp != null) cmisPwd = cmisPwdProp;
+
+    BaseCMISMacro baseMacro = new BaseCMISMacro() {
+
+      public boolean hasBody() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+      }
+
+      public RenderMode getBodyRenderMode() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+      }
+
+      @Override
+      protected String executeImpl(Map params, String body, RenderContext renderContext, ConfluenceCMISRepository repo) {
+        return "OK";
+      }
+    };
+
+    baseMacro.setBandanaManager(new MockBandanaManager());
+
+    String result = null;
+
+    try {
+      result = baseMacro.execute(parameters, null, null);
+    }
+    catch (MacroException me) {
+      logger.error(me);
+      fail(me.getMessage());
+    }
+
+    assertNotNull(result);
+    assertFalse("".equals(result));
+
+    logger.debug(result);
+  }
+
+  public void testRepositoryEnumeration() {
+
+    RepositoryStorage repoStorage = RepositoryStorage.getInstance(new MockBandanaManager());
+
+    Set<String> repos = repoStorage.getRepositoryNames();
+
+    assertNotNull(repos);
+    assertTrue(!repos.isEmpty());
+
+    for (String repo : repos) {
+      try {
+        ConfluenceCMISRepository repoDesc = repoStorage.getRepository(repo);
+
+        logger.debug("name: " + repoDesc.getName());
+        logger.debug("id: " + repoDesc.getRepository().getId());
+        logger.debug("productName : " + repoDesc.getRepository().getProductName());
+        logger.debug("cmisVersionSupported: " + repoDesc.getRepository().getCmisVersionSupported());
+        logger.debug("description: " + repoDesc.getRepository().getDescription());
+      }
+      catch (CmisRuntimeException e) {
+        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        fail(e.getMessage());
+      }
+    }
+
+  }
+
+  public void testCMISLinkGeneration() {
+    RepositoryStorage repoStorage = RepositoryStorage.getInstance(new MockBandanaManager());
+
+    try {
+      ConfluenceCMISRepository repo = repoStorage.getRepository("test");
+      Session session = repo.getRepository().createSession();
+      ItemIterable<CmisObject> children = session.getRootFolder().getChildren();
+      for (CmisObject obj : children) {
+        if ("TODO.txt".equals(obj.getName())) {
+          Document doc = (Document) obj;
+          System.out.println(doc);
+        }
+      }
+    }
+    catch (CmisRuntimeException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+      fail(e.getMessage());
+    }
+  }
+
+  class MockBandanaManager implements BandanaManager {
+
+    public void init() {
+      //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void setValue(BandanaContext bandanaContext, String s, Object o) {
+      //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    // used in RepositoryStorage
+
+    public Object getValue(BandanaContext bandanaContext, String s) {
+      Map<String, List<String>> repoConfigs = new WeakHashMap<String, List<String>>();
+      List<String> repoConfig = new ArrayList<String>();
+
+      repoConfig.add(cmisRealm);
+      repoConfig.add(cmisUser);
+      repoConfig.add(cmisPwd);
+      //No need to specify a RepositoryID
+      repoConfig.add(null);
+
+      repoConfigs.put("test", repoConfig);
+
+      return repoConfigs;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public Object getValue(BandanaContext bandanaContext, String s, boolean b) {
+      return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public String exportValues(BandanaContext bandanaContext) {
+      return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void importValues(BandanaContext bandanaContext, String s) {
+      //To change body of implemented methods use File | Settings | File Templates.
+    }
+  }
+}
