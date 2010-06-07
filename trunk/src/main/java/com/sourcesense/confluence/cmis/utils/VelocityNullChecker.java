bd47a11a613ef4470645359c4b37bc813966d16d
c.sciolla@sourcesense.com <c.sciolla@sourcesense.com@83e5ee9e-5015-11de-ad91-37317df011ef>
2010-06-07 15:25:49 +0000

    Provide a velocity object for null checks

---
+++ trunk/src/main/java/com/sourcesense/confluence/cmis/utils/VelocityNullChecker.java
@@ -0,0 +1,12 @@
+package com.sourcesense.confluence.cmis.utils;
+
+/**
+ * @author Carlo Sciolla &lt;c.sciolla@sourcesense.com&gt;
+ */
+public class VelocityNullChecker
+{
+    public final boolean isNull (Object obj)
+    {
+        return obj == null;
+    }
+}
